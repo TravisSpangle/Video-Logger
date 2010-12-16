45442a2df2d5f24eceafc5c88e849ad2f7c2b4da
Travis <spangltk@gmail.com>
2010-12-16 12:19:46 -0800

    Created project

---
+++ MyDocument.h
@@ -0,0 +1,14 @@
+//
+//  MyDocument.h
+//  Video-Logger
+//
+//  Created by Travis Spangle on 12/16/10.
+//  Copyright __MyCompanyName__ 2010 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSPersistentDocument {
+}
+
+@end
---
+++ MyDocument.m
@@ -0,0 +1,33 @@
+//
+//  MyDocument.m
+//  Video-Logger
+//
+//  Created by Travis Spangle on 12/16/10.
+//  Copyright __MyCompanyName__ 2010 . All rights reserved.
+//
+
+#import "MyDocument.h"
+
+@implementation MyDocument
+
+- (id)init 
+{
+    self = [super init];
+    if (self != nil) {
+        // initialization code
+    }
+    return self;
+}
+
+- (NSString *)windowNibName 
+{
+    return @"MyDocument";
+}
+
+- (void)windowControllerDidLoadNib:(NSWindowController *)windowController 
+{
+    [super windowControllerDidLoadNib:windowController];
+    // user interface preparation code
+}
+
+@end
